e2b406ecae7d87f0d39a771ccad4c92700aa1a86
Harry Duce <harry.duce@gmail.com>
2015-12-08 15:47:55 +0000

    initial commit of SearchViewController

---
+++ mob-app-project/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.wift.swift
+//  mob-app-project
+//
+//  Created by Harry Duce on 08/12/2015.
+//  Copyright © 2015 HarryFraser. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
