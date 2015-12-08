c5b8f613734ba113d13203d1d18fbb1ec1f57216
Fraser Murray <fraser.m.murray@gmail.com>
2015-12-08 15:23:36 +0000

    initial commit for Dota2API

---
+++ mob-app-project/Dota2API.swift
@@ -0,0 +1,22 @@
+//
+//  Dota2API.swift
+//  mob-app-project
+//
+//  Created by Fraser Murray on 08/12/2015.
+//  Copyright © 2015 HarryFraser. All rights reserved.
+//
+
+import Foundation
+
+struct MatchHistory {
+    let numResults: Int
+    let totalResults: Int
+    let remainingResults: Int
+    let matches: [Match]
+}
+
+typealias MatchID = Int
+
+struct Match {
+    let matchID: MatchID
+}
