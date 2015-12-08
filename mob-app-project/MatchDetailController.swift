4239152f8de6863aba58770b0ff9a1e6615eea08
Harry Duce <harry.duce@gmail.com>
2015-12-08 17:06:16 +0000

    add MatchDetailController.swift

---
+++ mob-app-project/MatchDetailController.swift
@@ -0,0 +1,14 @@
+//
+//  MatchDetailController.swift
+//  mob-app-project
+//
+//  Created by Harry Duce on 08/12/2015.
+//  Copyright © 2015 HarryFraser. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MatchDetailController: UIViewController {
+    
+}
