e7ad6912cbe58ebddbbf344b32abe64ac2ea3dec
Harry Duce <harry.duce@gmail.com>
2015-12-08 16:30:47 +0000

    add UserMatchHistory ViewController class

---
+++ mob-app-project/UserMatchHistory.swift
@@ -0,0 +1,18 @@
+//
+//  UserMatchHistory.swift
+//  mob-app-project
+//
+//  Created by Harry Duce on 08/12/2015.
+//  Copyright © 2015 HarryFraser. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserMatchHistory: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
