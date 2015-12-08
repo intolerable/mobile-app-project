139cc80afe2a1d4603b93bfc3bcbffe35dc0a61e
Harry Duce <harry.duce@gmail.com>
2015-12-08 16:41:06 +0000

    Change SearchViewController to UITableViewController

--- mob-app-project/UserMatchHistory.swift
+++ mob-app-project/UserMatchHistory.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class UserMatchHistory: UIViewController {
+class UserMatchHistory: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
