1d061a5749aca22a841782e2db3758f3acdbf16d
Harry Duce <harry.duce@gmail.com>
2015-12-09 16:56:03 +0000

    add tableView fill functions to MatchDetailController

--- mob-app-project/MatchDetailController.swift
+++ mob-app-project/MatchDetailController.swift
@@ -9,11 +9,46 @@
 import Foundation
 import UIKit
 
-class MatchDetailController: UIViewController {
+class MatchDetailController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
     
     // MARK: Properties
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var victoryLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var team1Table: UITableView!
     @IBOutlet weak var team2Table: UITableView!
+    
+    let team1 = ["Player 1", "Player 2", "Player 3", "Player 4", "Player 5"]
+    let team2 = ["Player 1", "Player 2", "Player 3", "Player 9", "Player 10"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView.isEqual(team1Table) {
+            return team1.count
+        }
+        else {
+            return team2.count
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if tableView.isEqual(team1Table){
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("team1Player")! as UITableViewCell
+            
+            cell.textLabel?.text = team1[indexPath.row]
+            
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("team2Player")! as UITableViewCell
+            
+            cell.textLabel?.text = team2[indexPath.row]
+            
+            return cell
+        }
+    }
 }
