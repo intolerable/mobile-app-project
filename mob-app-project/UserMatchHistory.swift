49bf2586293269f4e3acb0d5f5fafc8a744cc662
Fraser Murray <fraser.m.murray@gmail.com>
2015-12-09 16:46:05 +0000

    add loading for match history

--- mob-app-project/UserMatchHistory.swift
+++ mob-app-project/UserMatchHistory.swift
@@ -11,23 +11,41 @@ import UIKit
 
 class UserMatchHistory: UITableViewController {
     
-    let transportItems = ["Bus","Helicopter","Truck","Boat","Bicycle","Motorcycle","Plane","Train","Car","Scooter","Caravan"]
+    var matches: [Match] = []
+    
+    var requestedAccountID: AccountID?
 
     @IBOutlet var historyView: UITableView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        if
+            let path = NSBundle.mainBundle().pathForResource("Keys", ofType: "plist"),
+            let dict = NSDictionary(contentsOfFile: path) as? Dictionary<String, AnyObject> {
+                retrieveJSON(NSURL(string: apiURL(dict["api key"] as! String))!) { x in
+                    switch x {
+                    case let Either.Left(err):
+                        print(err)
+                    case let Either.Right(j):
+                        if let mh = parseMatchHistory(j as! [String: AnyObject]) {
+                            self.matches = mh.matches
+                            dispatch_async(dispatch_get_main_queue()) {
+                                self.historyView.reloadData()
+                            }
+                        }
+                    }
+                }
+        }
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return transportItems.count
+        return matches.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("prototypeCell1")! as UITableViewCell
         
-        cell.textLabel?.text = transportItems[indexPath.row]
+        cell.textLabel?.text = String(matches[indexPath.row])
         
         return cell
     }
